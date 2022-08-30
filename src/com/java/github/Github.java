0c0b88620ed075723c27d2f88ca8feb0bd3d1322
vsathiyanaray <vsathiyanaray@ITEM-S119974.emea.msad.sopra>
2022-08-30 12:19:08 +0530

    java project commit (first)

---
+++ src/com/java/github/Github.java
@@ -0,0 +1,10 @@
+package com.java.github;
+
+public class Github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module GithubProjectV1 {
+}
